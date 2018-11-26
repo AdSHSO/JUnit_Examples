00bbb02bbf8dc6f4fd919977c1f1a9a8c1fb14ee
AdSHSO <44525339+AdSHSO@users.noreply.github.com>
2018-11-26 18:33:05 +0100

    added
    added

---
+++ Account/src/main/java/testpackage/Account.java
@@ -0,0 +1,24 @@
+package testpackage;
+
+/**
+ * Hello world!
+ *
+ */
+public class Account {
+
+	private int balance;
+	
+	Account(int x){
+		this.balance = x;
+	}
+	
+	int withdraw(int value) {
+		
+		if (balance - value < 0) {
+			throw new IllegalArgumentException("You can only withdraw up to " + balance);
+		}
+		this.balance = balance - value;
+		return this.balance;
+	}
+	
+}
---
+++ Account/src/test/java/testpackage/withdrawTest.java
@@ -0,0 +1,23 @@
+package testpackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class withdrawTest {
+
+		@Test(expected = IllegalArgumentException.class)
+		public void testExceptionIsThrown() {
+		  Account tester = new Account(500);
+		  tester.withdraw(600);
+		}
+
+		@Test
+		public void testWithdraw() {
+		Account tester = new Account(500);
+		  assertEquals("300 withdrawn from 500 must be 200", 200, tester.withdraw(300));
+		}
+
+}
+
+
